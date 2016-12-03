1bc9b93184ebbc8b0636a59ff66164d01ce6f0df
Richard Senrosius <mrrose@Richards-MBP.home>
2016-12-02 21:38:44 -0500

    kind works

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,45 +1,57 @@
 var ranNums = Math.floor(Math.random() * 100);
 var numsToAdd = []
 
-var red;
-var blue;
-var yellow;
-var green;
+// var red;
+// var blue;
+// var yellow;
+// var green;
+// var wins;
+// var losses;
 
 
-red = Math.floor(Math.random() * 11);
-blue = Math.floor(Math.random() * 6);
-yellow = Math.floor(Math.random() * 21);
-green = Math.floor(Math.random() * 2);
-
-numsToAdd = [ red + blue + yellow + green ];
-
+var red = Math.floor(Math.random() * 15);
+var blue = Math.floor(Math.random() * 6);
+var yellow = Math.floor(Math.random() * 21);
+var green = Math.floor(Math.random() * 2);
 var results = red + blue + yellow + green ;
+var wins = 0;
+var losses = 0;
 
-function randomWholeNum() {
-
-  // Only change code below this line.
+var numsToAdd = [ red + blue + yellow + green ];
 
 
-  return Math.random();
-}
+
+// function randomWholeNum() {
+
+//   // Only change code below this line.
+
+
+//   return Math.random();
+// }
 
 $('#randomNumber').html(ranNums);
 
 
 $('#totalScore').html(numsToAdd);
 
+
+
+
   if(numsToAdd == ranNums) {
 
+   wins ++
+   
    alert("you win");
 
   }
   else if (numsToAdd > ranNums) {
 
+  	losses++
+  	
   	alert ("You Lose")
   }
-
-  
+$('#winner').html(wins);
+$('#losser').html(losses);
 //   alert ("you lose")
 
 
